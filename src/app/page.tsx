c660d9888a6a1996ad9348f353459434b943daee
datnguyennnx <official.nguyendat@gmail.com>
2025-06-23 11:26:39 +0700

    chore: replace heroui by shadcnui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,8 @@
-import {Button} from "@heroui/button";
 
 export default function Home() {
   return (
-    <div className="flex flex-wrap gap-4 items-center">
-      <Button color="primary" variant="solid">
-        Solid
-      </Button>
-      <Button color="primary" variant="faded">
-        Faded
-      </Button>
-      <Button color="primary" variant="bordered">
-        Bordered
-      </Button>
-      <Button color="primary" variant="light">
-        Light
-      </Button>
-      <Button color="primary" variant="flat">
-        Flat
-      </Button>
-      <Button color="primary" variant="ghost">
-        Ghost
-      </Button>
-      <Button color="primary" variant="shadow">
-        Shadow
-      </Button>
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
 }
